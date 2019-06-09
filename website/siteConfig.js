0ce857ae6b68ab425acb1c637667f38785317bda
Aefyr <mcpetergorx@gmail.com>
2019-06-09 20:26:40 +0300

    fix siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'SovaScript', // Title for your website.
   tagline: 'Документация SovaScript',
   url: 'https://sovascript.github.io', // Your website URL
-  baseUrl: '/sovascript.github.io/', // Base URL for your project */
+  baseUrl: '/SovaScript/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'sovascript.github.io',
+  projectName: 'SovaScript',
   organizationName: 'Aefyr',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
